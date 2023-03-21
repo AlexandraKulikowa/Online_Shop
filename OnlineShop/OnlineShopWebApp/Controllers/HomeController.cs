8ec8d99cabd3083befe2937fa39069349a85a73c
AlexandraKulikowa <siriusblack@yandex.ru>
2023-03-21 16:23:43 +0300

    Сделала задание 2_2_1.

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using OnlineShopWebApp.Models;
+using Newtonsoft.Json;
 
 namespace OnlineShopWebApp.Controllers
-{
-    public class HomeController : Controller
+{    public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -18,11 +16,25 @@ namespace OnlineShopWebApp.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
-        }
+            var Products = new List<Product>()
+            {
+                new Product (1,"Картина Золотая осень", 8000, "Осенний пейзаж","Пейзаж", "масло","50*60",2022,false),
+                new Product (2,"Картина Пеннивайз", 6000, "Клоун из Оно","Портрет", "масло","25*30",2022,false),
+                new Product (3,"Картина Бокал вина", 3000, "Картина для оформления интерьера кухни","Натюрморт", "масло","20*25",2022,true),
+                new Product (4,"Картина Динозавр", 5000, "Тиранозавр Рекс","Пейзаж", "масло","30*35",2022,true),
+                new Product (5,"Картина Лара Крофт", 2000, "Анджелина Джоли в роли Лары Крофт","Портрет", "масло","20*25",2021,true),
+                new Product (6,"Картина Девушка и ветер", 4000, "Картина в подарок подруге","Портрет", "масло","20*25",2021,false),
+            };
 
+            var jsonProducts = JsonConvert.SerializeObject(Products);
+            System.IO.File.WriteAllText(@"jsonproducts.json", jsonProducts);
+            var ListOfProducts = JsonConvert.DeserializeObject<List<Product>>(jsonProducts);
+
+            var result = String.Join("", ListOfProducts);
+            return result;
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ OnlineShop/OnlineShopWebApp/Models/Products.cs
@@ -0,0 +1,33 @@
+﻿
+namespace OnlineShopWebApp.Models
+{
+    public class Product
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public string PaintingTechnique { get; set; }
+        public string Size { get; set; }
+        public int Year { get; set; }
+        public bool IsPromo { get; set; }
+
+        public Product(int id,string name, decimal cost, string description, string genre,  string paintingTechnique, string size, int year, bool ispromo)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+            Description = description;
+            Genre = genre;
+            PaintingTechnique = paintingTechnique;
+            Size = size;
+            Year = year;
+            IsPromo = ispromo;
+        }
+        public override string ToString() 
+        {
+            return $"{Id} \n{Name} \n{Cost} \n\n";
+        }
+}
+}
