c485381de9d47dd2dca5eb84c6184ead06a36346
AlexandraKulikowa <siriusblack@yandex.ru>
2023-04-05 13:34:58 +0300

    Сделана кнопка "Заказать" и сохранение корзины.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Interfaces;
+using OnlineShopWebApp.Repositories;
+using System;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IBasketRepository baskets;
+        private readonly IOrderRepository orders;
+        public OrderController(IBasketRepository baskets, IOrderRepository orders)
+        {
+            this.baskets = baskets;
+            this.orders = orders;
+        }
+        public IActionResult Index(Guid basketId)
+        {
+            var basketById = baskets.TryGetByBasketId(basketId);
+            return View(basketById);
+        }
+        public IActionResult ToCheckOut(Guid basketId)
+        {
+            var basketById = baskets.TryGetByBasketId(basketId);
+            orders.Add(basketById);
+            baskets.Clear(basketId);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Repositories/InMemoryOrdersRepository.cs
@@ -0,0 +1,21 @@
+﻿using OnlineShopWebApp.Interfaces;
+using System.Collections.Generic;
+
+namespace OnlineShopWebApp.Repositories
+{
+    public class InMemoryOrdersRepository : IOrderRepository
+    {
+        private List<Basket> orders = new List<Basket>();
+        public List<Basket> Orders
+        {
+            get
+            {
+                return orders;
+            }
+        }
+        public void Add(Basket basket)
+        {
+            orders.Add(basket);
+        }
+    }
+}
