14292681bd8d25b362835b8134e56153513910c0
AlexandraKulikowa <siriusblack@yandex.ru>
2023-04-25 13:05:38 +0300

    Исправлены замечания.

--- OnlineShop/OnlineShopWebApp/Interfaces/IFavouriteRepository.cs
+++ OnlineShop/OnlineShopWebApp/Interfaces/IFavouriteRepository.cs
@@ -1,14 +1,12 @@
 ﻿using OnlineShopWebApp.Models;
-using System.Collections.Generic;
 
 namespace OnlineShopWebApp.Interfaces
 {
     public interface IFavouriteRepository
     {
-        List<Favourites> FavouriteList { get; }
         Favourites TryGetByUserId(string userId);
         void Add(Product product, string userId);
-        void DeleteProduct(string userId, int id);
+        void DeleteFavourite(string userId, int id);
         void Clear(string userId);
     }
 }
